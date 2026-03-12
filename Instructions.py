b77d60de8d3c9477b910cf3ea751788fe171131a
emmnms2 <chen.emily2207@gmail.com>
2026-03-12 19:10:57 -0400

    adding instructions

---
+++ Instructions.py
@@ -0,0 +1,39 @@
+#Hello all!
+#This file is where you'll be able to access all the information and instructions for the activity
+#Note: Using a # allows you to comment notes instead of code.
+
+#The idea for the activity is for you to make your own branches and push them using
+#the git commands we just learned!
+
+#Here's the instructions
+#1) Start by making your own branch using these Git commands in Terminal:
+    #git checkout -b <branch-name>
+#Note: checkout -b creates AND switches you into the branch you created
+
+#2) In your branch, create a new file. Ex: Instructions.py.
+    #Don't forget the .py!
+#***If you were assigned a special team, then please click on the "merge_conflict.py" file!!!
+
+#3) Under these instructions, you'll find a list of descriptions of each of the career paths
+#we discussed. Copy the one you were assigned and paste them into the file you created.
+
+#4) Now, get ready to save your work and push them into your branch.
+
+#5) Use the following Git commands in the Terminal in the following order:
+    #git add .
+    #git commit -m "Your Message"
+    #git push origin <branch-name>
+
+#6) You should now be able to see your changes in GitHub under your branch!
+
+#Now, we will merge so you can practice pulling! Please wait here during the activity.
+#For special teams that were assigned the same descriptions, you should have experienced a merge error!
+#Don't worry -- we will showcase how to navigate a merge error!
+#Now for the final step!
+
+#7) In order to update your main, use these commands in the Terminal:
+    #git checkout main
+    #git pull origin main
+
+#Optional Cool Visual:
+    #git log --oneline --graph --all
